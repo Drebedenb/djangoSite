8c90a500cb09587b540eea51b2606f46621ba9f4
Drebedenb <danila.kuzya2001@gmail.com>
2022-06-06 23:14:43 +0300

    Change DB

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 
-
 DATABASES = {
 'default': {
 'ENGINE': 'django.db.backends.sqlite3',
@@ -128,10 +127,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(file))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
