5fb4b2318610526f3c6ad599206dfd1f76c12ea7
Drebedenb <danila.kuzya2001@gmail.com>
2022-06-06 21:25:22 +0300

    Add files for hiroku deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,5 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(file))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
