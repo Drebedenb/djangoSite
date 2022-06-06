7f14de0a3759758f6910619ba9cf370cef11be28
Drebedenb <danila.kuzya2001@gmail.com>
2022-06-06 21:56:09 +0300

    Change DB

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,16 +76,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-'USER': 'root',
-'PASSWORD': 'password',
-'HOST': 'db',
-'PORT': '3306',
-}
-}
+
+
+DATABASES = {'default': dj_database_url.config(default='postgres://user:pass@localhost/dbname')}
+
+
+
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# 'USER': 'root',
+# 'PASSWORD': 'password',
+# 'HOST': 'db',
+# 'PORT': '3306',
+# }
+# }
 
 
 # Password validation
